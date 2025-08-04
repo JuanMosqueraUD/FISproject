a20ed7784db6c701dc469314a396ecd34812d782
JuanMosqueraUD <juanpa.mosqueram@gmail.com>
2025-08-04 09:24:51 -0500

    port biding

--- run_server.py
+++ run_server.py
@@ -16,11 +16,12 @@ if __name__ == "__main__":
     print(f"Base de datos: {os.getenv('DATABASE_URL', 'No configurada')}")
     
     try:
+        port = int(os.environ.get("PORT", 8000))
         uvicorn.run(
             "main:app",
-            host="127.0.0.1",
-            port=8000,
-            reload=True,
+            host="0.0.0.0",
+            port=port,
+            reload=False,
             log_level="info"
         )
     except Exception as e:
